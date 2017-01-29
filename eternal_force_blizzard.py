cd102a935a9ef66cb534f99eab52188655e69064
kouki kawabata <kouki@dm.cs.kumamoto-u.ac.jp>
2017-01-29 19:16:01 +0900

    indent

--- eternal_force_blizzard.py
+++ eternal_force_blizzard.py
@@ -3,20 +3,19 @@ import tweepy
 import settings
 import oauth
 
-class MyStreamListener(tweepy.StreamListener):
-    def on_status(self, status):
-        if status.text[0] == '@':
-            return
-        if not status.favorited:
-            api.create_favorite(status.id)
+class MyStreamListener(tweepy.MyStreamListener):
+  def on_status(self, status):
+    if status.text[0] == '@': return
+    if not status.favorited:
+      api.create_favorite(status.id)
 
-    def on_error(self, status_code):
-        if status_code == 420:
-            return False
+  def on_error(self, status_code):
+    if status_code == 420:
+      return False
 
-if __name__ == "__main__":
-    db = oauth.load_database()
-    main_id = input('Your user id: ')
-    api = oauth.get_api(db, main_id)
-    myStream = tweepy.Stream(api.auth, MyStreamListener())
-    myStream.userstream()
+if __name__ == '__main__':
+  db = oauth.load_database()
+  main_id = input('Your user id: ')
+  api = oauth.get_api(db, main_id)
+  myStream = tweepy.Stream(api.auth, MyStreamListener())
+  myStream.userstream()
